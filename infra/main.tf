9d25f84922621e65da10451c596ad5a75c310c96
Scott DeVerinne <s.dev.enquiries@gmail.com>
2026-05-03 19:48:34 +0100

    Alter resource "azurerm_storage_account" "main"

--- infra/main.tf
+++ infra/main.tf
@@ -12,3 +12,17 @@ resource "azurerm_resource_group" "main" {
 
   tags = local.common_tags
 }
+
+resource "azurerm_storage_account" "main" {
+  name                = "eventplatformstoragesdd"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  account_tier             = "Standard"
+  account_replication_type = "RAGRS"
+
+  allow_nested_items_to_be_public  = false
+  cross_tenant_replication_enabled = false
+
+  tags = local.common_tags
+}
