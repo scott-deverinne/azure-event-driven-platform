ed489996cc4363d90bad14e8eb28c8f616255acb
Scott DeVerinne <s.dev.enquiries@gmail.com>
2026-05-04 12:16:26 +0100

    Terraform Manages: Resource Group Storage Account Blob Containers Service Bus Namespace Service Bus Queue Application Insights

--- infra/main.tf
+++ infra/main.tf
@@ -26,3 +26,63 @@ resource "azurerm_storage_account" "main" {
 
   tags = local.common_tags
 }
+
+resource "azurerm_storage_container" "events" {
+  name                  = "events"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "processed_events" {
+  name                  = "processed-events"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_container" "dead_letter" {
+  name                  = "dead-letter"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
+}
+resource "azurerm_servicebus_namespace" "main" {
+  name                = "event-platform-sb-scott"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  sku = "Standard"
+
+  tags = local.common_tags
+}
+resource "azurerm_servicebus_queue" "events" {
+  name         = "event-queue-dev"
+  namespace_id = azurerm_servicebus_namespace.main.id
+
+  lock_duration = "PT1M"
+
+  max_delivery_count = 10
+
+  default_message_ttl = "P14D"
+
+  duplicate_detection_history_time_window = "PT10M"
+
+  batched_operations_enabled = true
+  partitioning_enabled = false
+
+  dead_lettering_on_message_expiration = false
+  requires_duplicate_detection         = false
+  requires_session                     = false
+
+  max_size_in_megabytes = 1024
+}
+resource "azurerm_application_insights" "main" {
+  name                = "event-platform-ai"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  application_type    = "web"
+  sampling_percentage = 0
+
+  workspace_id = "/subscriptions/552d3bbc-1a07-49fa-9df2-d755c784c4e7/resourceGroups/DefaultResourceGroup-SUK/providers/Microsoft.OperationalInsights/workspaces/DefaultWorkspace-552d3bbc-1a07-49fa-9df2-d755c784c4e7-SUK"
+
+  tags = local.common_tags
+}
