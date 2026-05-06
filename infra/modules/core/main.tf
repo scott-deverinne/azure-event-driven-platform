dd4f2cc1764db5c824bc312f3765aada9cfd3e7b
Scott DeVerinne <s.dev.enquiries@gmail.com>
2026-05-06 11:30:28 +0100

    Productionize Azure infrastructure with Terraform

--- infra/modules/core/main.tf
+++ infra/modules/core/main.tf
@@ -44,6 +44,7 @@ resource "azurerm_storage_container" "dead_letter" {
   storage_account_name  = azurerm_storage_account.main.name
   container_access_type = "private"
 }
+
 resource "azurerm_servicebus_namespace" "main" {
   name                = "event-platform-sb-scott"
   location            = azurerm_resource_group.main.location
@@ -53,6 +54,7 @@ resource "azurerm_servicebus_namespace" "main" {
 
   tags = local.common_tags
 }
+
 resource "azurerm_servicebus_queue" "events" {
   name         = "event-queue-dev"
   namespace_id = azurerm_servicebus_namespace.main.id
@@ -66,7 +68,7 @@ resource "azurerm_servicebus_queue" "events" {
   duplicate_detection_history_time_window = "PT10M"
 
   batched_operations_enabled = true
-  partitioning_enabled = false
+  partitioning_enabled       = false
 
   dead_lettering_on_message_expiration = false
   requires_duplicate_detection         = false
@@ -74,6 +76,7 @@ resource "azurerm_servicebus_queue" "events" {
 
   max_size_in_megabytes = 1024
 }
+
 resource "azurerm_application_insights" "main" {
   name                = "event-platform-ai"
   location            = azurerm_resource_group.main.location
@@ -102,7 +105,14 @@ resource "azurerm_key_vault" "main" {
   soft_delete_retention_days = 90
 
   tags = local.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      contact
+    ]
+  }
 }
+
 resource "azurerm_service_plan" "api" {
   name                = "ASP-eventplatformrg-a2f8"
   location            = "westeurope"
@@ -124,6 +134,7 @@ resource "azurerm_service_plan" "function" {
 
   tags = local.common_tags
 }
+
 resource "azurerm_windows_web_app" "api" {
   name                = "event-platform-api-scott-dev"
   location            = "westeurope"
@@ -132,8 +143,8 @@ resource "azurerm_windows_web_app" "api" {
 
   https_only = true
 
-  client_affinity_enabled                  = true
-  ftp_publish_basic_authentication_enabled = false
+  client_affinity_enabled                       = true
+  ftp_publish_basic_authentication_enabled      = false
   webdeploy_publish_basic_authentication_enabled = false
 
   site_config {
@@ -151,7 +162,12 @@ resource "azurerm_windows_web_app" "api" {
       site_config[0].virtual_application,
     ]
   }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
+
 resource "azurerm_windows_function_app" "function" {
   name                = "event-platform-func-scott-dev"
   location            = "westeurope"
@@ -186,4 +202,71 @@ resource "azurerm_windows_function_app" "function" {
       site_config[0].cors,
     ]
   }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
+
+resource "azurerm_key_vault_access_policy" "api" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_windows_web_app.api.identity[0].principal_id
+
+  secret_permissions = [
+    "Get",
+    "List"
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "function" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = azurerm_windows_function_app.function.identity[0].principal_id
+
+  secret_permissions = [
+    "Get",
+    "List"
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "terraform_user" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete"
+  ]
+
+  certificate_permissions = [
+    "Get",
+    "List",
+    "ManageContacts"
+  ]
+}
+
+# Temporarily managing secrets outside Terraform due to Azure Key Vault auth issue
+#
+# resource "azurerm_key_vault_secret" "servicebus" {
+#   name         = "servicebus-connection"
+#   value        = azurerm_servicebus_namespace.main.default_primary_connection_string
+#   key_vault_id = azurerm_key_vault.main.id
+#
+#   depends_on = [
+#     azurerm_key_vault_access_policy.terraform_user
+#   ]
+# }
+#
+# resource "azurerm_key_vault_secret" "storage" {
+#   name         = "storage-connection"
+#   value        = azurerm_storage_account.main.primary_connection_string
+#   key_vault_id = azurerm_key_vault.main.id
+#
+#   depends_on = [
+#     azurerm_key_vault_access_policy.terraform_user
+#   ]
+# }
